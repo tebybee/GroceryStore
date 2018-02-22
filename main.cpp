825fbd398084ef5cee24f433fea5245a12039dad
tebybee <tbybee@mail.csuchico.edu>
2018-02-22 11:29:57 -0800

    prompts the user with the grocery list interface each time they enter something other than 'q' or 'Q'.

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+//author: Travis Bybee
 #include<iostream>
 #include<string>
 using namespace std;
@@ -7,13 +8,15 @@ int main()
   string list[5]; //array of 5 strings
   int numItems = 0;
   char input;
-
+ do
+ {
   cout<<"\n==GROCERY LIST MANAGER==";
   cout<<"\nEnter your choice: ";
   cout<<"\n (A)dd an item";
   cout<<"\n (Q)uit";
   cout<<"\nYour choice (A/Q): ";
   cin>>input;
-
-  return 0;
+ }while(input!='Q'&& input!='q');
+ return 0;
 }
+
