bba6306fc189c620eee7ad86d8fd8af6068a3bfe
tebybee <tbybee@mail.csuchico.edu>
2018-02-27 11:25:24 -0800

    grocery list that fits a certain number of items in it and can be continuously added to if there is available room

--- main.cpp
+++ main.cpp
@@ -8,15 +8,31 @@ int main()
   string list[5]; //array of 5 strings
   int numItems = 0;
   char input;
+  string Item;
  do
  {
   cout<<"\n==GROCERY LIST MANAGER==";
   cout<<"\nEnter your choice: ";
   cout<<"\n (A)dd an item";
   cout<<"\n (Q)uit";
-  cout<<"\nYour choice (A/Q): ";
+  cout<<"\n Your choice (A/Q): ";
   cin>>input;
- }while(input!='Q'&& input!='q');
+ 
+  if(input=='A'||input=='a')
+  {
+    cout<<"What is the item?\n";
+    if(numItems==5)
+    {
+     cout<<"You'll need a bigger list!\n";
+    } 
+    else
+    {
+     cin>>Item;
+     list[numItems]=Item;
+     numItems++;
+    }
+  }
+ }while(input != 'q' && input != 'Q');
  return 0;
 }
 
