72cd55460c7c181de8520fc90dfabf27be51dcb7
tebybee <tbybee@mail.csuchico.edu>
2018-03-01 11:17:15 -0800

    grocery list that allows user to add to the list if there is available space. prints out the list if the user exits the list.

--- main.cpp
+++ main.cpp
@@ -5,44 +5,44 @@ using namespace std;
 
 int main()
 {
-  int LIST_SIZE = 5;
-  string list[LIST_SIZE]; //array of 5 strings
-  int numItems = 0;
-  char input;
-  string Item;
- do
- {
-  cout<<"\n==GROCERY LIST MANAGER==";
-  cout<<"\nEnter your choice: ";
-  cout<<"\n (A)dd an item";
-  cout<<"\n (Q)uit";
-  cout<<"\nYour choice (A/Q): ";
-  cin>>input;
- 
-  if(input=='A'||input=='a')
-  {
-    cout<<"What is the item?\n";
-    if(numItems==5)
-    {
-     cout<<"You'll need a bigger list!\n";
-    } 
-    else
-    {
-     cin>>Item;
-     list[numItems]=Item;
-     numItems++;
-    }
-  }
- }while(input != 'q' && input != 'Q');
- if(input == 'q' || input == 'Q')
- {
-   cout<<"==ITEMS TO BUY==\n";
-  for(int x=0; x<LIST_SIZE; x++)
-  {
-     cout<<x+1;
-     cout<<" "<<list[x]<<"\n";
-  }
+        int LIST_SIZE = 5;
+        string list[LIST_SIZE]; //array of 5 strings
+        int numItems = 0;
+        char input;
+        string Item;
+        do
+        {
+                cout<<"\n==GROCERY LIST MANAGER==";
+                cout<<"\nEnter your choice: ";
+                cout<<"\n (A)dd an item";
+                cout<<"\n (Q)uit";
+                cout<<"\nYour choice (A/Q): ";
+                cin>>input;
 
- }
- return 0;
+                if(input=='A'||input=='a')
+                {
+                        cout<<"What is the item?\n";
+                        cin>>input; 
+                        if(numItems==5)
+                        {
+                                cout<<"You'll need a bigger list!\n";
+                        } 
+                        else
+                        {
+                                list[numItems]=Item;
+                                numItems++;
+                        }
+                }
+        }while(input != 'q' && input != 'Q');
+        if(input == 'q' || input == 'Q')
+        {
+                cout<<"==ITEMS TO BUY==\n";
+                for(int x=0; x<LIST_SIZE; x++)
+                {
+                        cout<<x+1;
+                        cout<<" "<<list[x]<<"\n";
+                }
+
+        }
+        return 0;
 }
