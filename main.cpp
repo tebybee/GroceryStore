d1b63cb50b4d07d98a1ed858abe2991e1ac63bbd
tebybee <tbybee@mail.csuchico.edu>
2018-03-13 11:37:53 -0700

    changed the Grocerylist program to use vectors, which allows the list to be infinite size, increasing it when it need to. if there are no items in the list and the user quits it will say that there are no items to buy. The list will print at the end when the user quits.

--- main.cpp
+++ main.cpp
@@ -1,13 +1,12 @@
 //author: Travis Bybee
 #include<iostream>
 #include<string>
+#include<vector>
 using namespace std;
 
 int main()
 {
-        int LIST_SIZE = 5;
-        string list[LIST_SIZE]; //array of 5 strings
-        int numItems = 0;
+        vector <string> List; 
         char input;
         string Item;
         do
@@ -22,25 +21,24 @@ int main()
                 if(input=='A'||input=='a')
                 {
                         cout<<"What is the item?\n";
-                        cin>>input; 
-                        if(numItems==5)
-                        {
-                                cout<<"You'll need a bigger list!\n";
-                        } 
-                        else
-                        {
-                                list[numItems]=Item;
-                                numItems++;
-                        }
+                        cin>>Item;
+                        List.push_back(Item);
                 }
         }while(input != 'q' && input != 'Q');
         if(input == 'q' || input == 'Q')
         {
-                cout<<"==ITEMS TO BUY==\n";
-                for(int x=0; x<LIST_SIZE; x++)
+                if(List.size()>=1)
                 {
-                        cout<<x+1;
-                        cout<<" "<<list[x]<<"\n";
+                        cout<<"==ITEMS TO BUY==\n";
+                        for(int x=0; x<List.size(); x++)
+                        {
+                                cout<<x+1;
+                                cout<<" "<<List[x]<<"\n";
+                        }
+                }
+                else
+                {
+                        cout<<"No items to buy!\n";
                 }
 
         }
